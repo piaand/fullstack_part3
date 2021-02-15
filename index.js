4e3108ed286b231a5de7129d275984a955e4293f
piaand <pia.m.andersin@gmail.com>
2021-02-15 16:24:31 +0200

    status after exercise 3.3.

--- index.js
+++ index.js
@@ -42,6 +42,16 @@ app.get('/info', (request, response) => {
 	response.send(`<p>${text}<br><br>${date}</p>`)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id)
+	const person = persons.find(person => person.id === id)
+	if(!person) {
+		return response.status(404).end();
+	}
+
+	response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
 	console.log(`Server running  ${PORT}`)
